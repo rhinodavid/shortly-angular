731b2a6f744e9be98177dcb903d36989269d42e8
rhinodavid <dawalsh@gmail.com>
2016-10-14 16:22:46 -0700

    Redirect to links after creating new link

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,12 +1,10 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  $scope.link = {};
-
   $scope.addLink = function(url) {
     Links.addOne({url: url})
     .then(function(resp) {
-      // console.log(resp);
+      $location.url('/links');
     });
   };
 });
