3d3d8c4fa7b0deece7abb332d5a88e6689aebb93
rhinodavid <dawalsh@gmail.com>
2016-10-14 16:20:06 -0700

    Add authentication on links and shorten routes

--- client/app/app.js
+++ client/app/app.js
@@ -17,11 +17,13 @@ angular.module('shortly', [
     })
     .when('/links', {
       templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
+      controller: 'LinksController',
+      authenticate: true
     })
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
-      controller: 'ShortenController'
+      controller: 'ShortenController',
+      authenticate: true
     });
 
     // We add our $httpInterceptor into the array
@@ -53,9 +55,12 @@ angular.module('shortly', [
   // when it does change routes, we then look for the token in localstorage
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
+
   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
       $location.path('/signin');
     }
   });
+}).run(function($rootScope, $location) {
+  $location.path('/links');
 });
