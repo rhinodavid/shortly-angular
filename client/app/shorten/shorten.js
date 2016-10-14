2399578620332f2594aca8e77250f84882432334
rhinodavid <dawalsh@gmail.com>
2016-10-14 15:05:50 -0700

    Implement shorten controller

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,12 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+  $scope.link = {};
+
+  $scope.addLink = function(url) {
+    Links.addOne({url: url})
+    .then(function(resp) {
+      // console.log(resp);
+    });
+  };
 });
